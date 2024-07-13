95ca021f9a332dd9b6d0cc6beade6e9af9ebf508
Eugene Lebedev <duke.tougu@gmail.com>
2024-07-13 12:06:11 +0300

    Add axum app skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("MAIL UTILITIES WIZARD");
+pub mod config;
+
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "MAIL UTILITIES WIZARD v0.1.0"
 }
