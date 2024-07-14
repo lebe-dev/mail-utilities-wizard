e98a6a9fc7f3ba66958c0643fd45f55c306fd2c0
Eugene Lebedev <duke.tougu@gmail.com>
2024-07-14 12:24:46 +0300

    Add configuration to serve embedded SPA

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use axum::{
     Router, routing::get,
 };
+use axum::http::{header, StatusCode, Uri};
+use axum::response::{Html, IntoResponse, Response};
+use rust_embed::Embed;
 
 use crate::config::file::loading_config_from_file;
 use crate::logging::get_logging_config;
@@ -18,6 +21,8 @@ pub mod tests;
 
 pub const VERSION: &str = "0.1.0 #UNKNOWN";
 
+static INDEX_HTML: &str = "index.html";
+
 #[tokio::main]
 async fn main() {
     let config = loading_config_from_file("config.yml")
@@ -31,19 +36,52 @@ async fn main() {
     }
 
     let app = Router::new()
-        .route("/", get(root))
-        .route("/api/version", get(get_version_route));
+        .route("/api/version", get(get_version_route)).fallback(static_handler);
 
-    let listener = tokio::net::TcpListener::bind(
-        format!("127.0.0.1:{}", config.port)
-    ).await.unwrap();
+    let bind = format!("127.0.0.1:{}", config.port);
+
+    let listener = tokio::net::TcpListener::bind(&bind).await.unwrap();
 
     println!("MAIL UTILITIES WIZARD v{VERSION}");
-    println!("URL: http://127.0.0.1:{}", config.port);
+    println!("URL: http://{bind}");
 
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn root() -> &'static str {
-    "UNDER DEVELOPMENT"
+async fn index_html() -> Response {
+    match Assets::get(INDEX_HTML) {
+        Some(content) => Html(content.data).into_response(),
+        None => not_found().await,
+    }
 }
+
+async fn static_handler(uri: Uri) -> impl IntoResponse {
+    let path = uri.path().trim_start_matches('/');
+
+    if path.is_empty() || path == INDEX_HTML {
+        return index_html().await;
+    }
+
+    match Assets::get(path) {
+        Some(content) => {
+            let mime = mime_guess::from_path(path).first_or_octet_stream();
+
+            ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
+        }
+        None => {
+            if path.contains('.') {
+                return not_found().await;
+            }
+
+            index_html().await
+        }
+    }
+}
+
+async fn not_found() -> Response {
+    (StatusCode::NOT_FOUND, "404").into_response()
+}
+
+#[derive(Embed)]
+#[folder = "static/"]
+struct Assets;
