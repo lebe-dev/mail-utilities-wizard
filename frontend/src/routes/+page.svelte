ca7dab65d7cbb8856108648f49afce43fb0b11aa
Eugene Lebedev <duke.tougu@gmail.com>
2024-07-14 12:24:20 +0300

    Add frontend module skeleton

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>UNDER DEVELOPMENT</title>
+	<meta name="description" content="Mail Utilities Wizard" />
+</svelte:head>
+
+<section>
+	UNDER DEVELOPMENT
+</section>
+
+<style>
+</style>
