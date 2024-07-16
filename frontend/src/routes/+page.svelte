810b1cd3ee99de754a2bb4b4f133bf5c4b67a7c6
Eugene Lebedev <duke.tougu@gmail.com>
2024-07-16 17:52:41 +0300

    Frontend: load config

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
 	import type {PageData} from "./$types";
+	import {fetchAppConfig} from "$lib/api";
+	import type {AppConfig} from "$lib/config";
 
 	export let data: PageData;
+
+	let pageLoading = true;
+	let unexpectedError = false;
+
+	fetchAppConfig().then((config: AppConfig) => {
+		console.log('config:', config);
+		data.config = config;
+		pageLoading = false;
+
+	}).catch((e: any) => {
+		console.error('unable to get config:', e);
+		pageLoading = false;
+		unexpectedError = true;
+	})
 </script>
 
 <svelte:head>
@@ -10,7 +26,12 @@
 </svelte:head>
 
 <section>
+	{#if !pageLoading}
 	<h1>{data.config.page.header}</h1>
+	<div>{data.config.page.selectLocationText}</div>
+	{:else if unexpectedError}
+		<div>Unexpected application error</div>
+	{/if}
 </section>
 
 <style>
