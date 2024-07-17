a4b727dcf74e078c4bae2c6da9dad8ec3b12c2c7
Eugene Lebedev <duke.tougu@gmail.com>
2024-07-17 17:58:25 +0300

    Frontend: add sendCounterData function

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -12,3 +12,35 @@ export async function fetchAppConfig(): Promise<AppConfig> {
         throw new Error('config load error')
     }
 }
+
+export async function sendCounterData(locationName: string, counterName: string, month: string, counterValue: string): Promise<AppConfig> {
+    const report = new ReportCounterData(locationName, counterName, month, counterValue);
+
+    const response = await fetch('/api/counter', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(report)
+    });
+
+    if (response.status === 200) {
+        return response.json()
+
+    } else {
+        throw new Error('config load error')
+    }
+}
+
+export class ReportCounterData {
+    constructor(locationName: string, counterName: string, month: string, counterValue: string) {
+        this.locationName = locationName;
+        this.counterName = counterName;
+        this.month = month;
+        this.counterValue = counterValue;
+    }
+    locationName: string;
+    counterName: string;
+    month: string;
+    counterValue: string;
+}
